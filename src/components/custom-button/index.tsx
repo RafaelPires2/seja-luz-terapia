f47421cfb82a05c4b906e2bc71fca382ac5d6657
RafaelPires2 <rafaelbrpires@gmail.com>
2023-04-01 10:53:18 -0300

    feature: custom button

--- src/components/custom-button/index.tsx
+++ src/components/custom-button/index.tsx
@@ -1,7 +1,37 @@
-export function CustomButton() {
-    return (
-      <>
-        <h1>Button</h1>
-      </>
-    );
-  }
+import { ButtonHTMLAttributes } from "react";
+import { ButtonContainer } from "./styles";
+
+type ButtonTypeProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+type ButtonContainerProps = {
+  width: string;
+  height: string;
+  textColor: string;
+  content: string;
+  variant: any;
+  type: "submit" | "button" | "reset";
+} & ButtonTypeProps;
+
+export function CustomButton({
+  width,
+  height,
+  textColor,
+  content,
+  type,
+
+  ...props
+}: ButtonContainerProps) {
+  return (
+    <>
+      <ButtonContainer
+        width={width}
+        height={height}
+        textColor={textColor}
+        type={type}
+        {...props}
+      >
+        {content}
+      </ButtonContainer>
+    </>
+  );
+}
