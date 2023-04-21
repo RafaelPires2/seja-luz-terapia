a13e0e38ae7da81ae7ba4079a863dbcadbb9af1b
RafaelPires2 <rafaelbrpires@gmail.com>
2023-04-21 00:07:34 -0300

    docs: router about

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
-import { About } from './pages/about';
 
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
     </Routes>
   );
 }
