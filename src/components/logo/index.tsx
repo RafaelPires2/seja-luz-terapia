21bb30612a3cb735f1fca738ab919eaafab502dc
RafaelPires2 <rafaelbrpires@gmail.com>
2023-04-21 12:09:07 -0300

    fix: img logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,7 +3,7 @@ import { ContainerLogo } from './styles';
 export function Logo() {
   return (
     <ContainerLogo>
-      <img src='./public/logo.png' alt='logo seja luz' />
+      <img src='/logo.png' alt='logo seja luz' />
     </ContainerLogo>
   );
 }
