c2c9b21826b965ad7f9a9b76b45f3cdb27f0cce8
RafaelPires2 <rafaelbrpires@gmail.com>
2023-04-21 19:30:59 -0300

    refactor: texts evidences

--- src/components/carrousel/index.tsx
+++ src/components/carrousel/index.tsx
@@ -25,7 +25,7 @@ export default function App() {
               <div className='content-profile'>
                 <div className='text-profile'>
                   <h3>Juliana Siqueira</h3>
-                  <p>TERAPIA CRIATIVA TRANSPESSOAL - PROFESSORA - POÁ/SP</p>
+                  <p>TERAPIA CRIATIVA TRANSPESSOAL - PROFESSORA - SANTANA/SP</p>
                 </div>
               </div>
               <p className='profile-description'>
@@ -41,13 +41,14 @@ export default function App() {
                 <div className='content-profile'>
                   <div className='text-profile'>
                     <h3>Vitória Pitta</h3>
-                    <p>TERAPIA CRIATIVA TRANSPESSOAL - ANALISTA DE DADOS - TRÊS PONTES/MG</p>
+                    <p>REIKI - ANALISTA DE DADOS - TRÊS PONTES/MG</p>
                   </div>
                 </div>
                 <p className='profile-description'>
-                  "Vá, estou muito feliz por ter feito a terapia criativa transpessoal com você. Foi uma experiência única que me ajudou a
-                  olhar para dentro de mim de uma forma nova e profunda. Me sinto mais inspirada e motivada para explorar minha mente e
-                  emoções. Muito obrigada por compartilhar sua expertise e me ajudar em minha jornada de autoconhecimento!"
+                  "Vá, eu simplesmente não consigo encontrar palavras para expressar minha gratidão pela experiência de Reiki que tive com
+                  você. Foi uma jornada de cura profunda e renovadora para minha mente, corpo e alma. Senti uma sensação de paz e equilíbrio
+                  que há muito tempo não sentia. Obrigada por compartilhar seu dom de cura comigo e me ajudar em minha jornada de bem-estar.
+                  Sou imensamente grata por ter tido essa experiência única!"
                 </p>
               </div>
             </div>
@@ -58,13 +59,13 @@ export default function App() {
                 <div className='content-profile'>
                   <div className='text-profile'>
                     <h3>Graziella Novaes</h3>
-                    <p>TERAPIA CRIATIVA TRANSPESSOAL - PERSONAL TRAINER - TRÊS PONTES/MG</p>
+                    <p>TCT - PERSONAL TRAINER - MOGI DAS CRUZES/SP</p>
                   </div>
                 </div>
                 <p className='profile-description'>
-                  "Vá, estou muito feliz por ter feito a terapia criativa transpessoal com você. Foi uma experiência única que me ajudou a
-                  olhar para dentro de mim de uma forma nova e profunda. Me sinto mais inspirada e motivada para explorar minha mente e
-                  emoções. Muito obrigada por compartilhar sua expertise e me ajudar em minha jornada de autoconhecimento!"
+                  "Quero expressar minha gratidão a você! Vá, MUITO OBRIGADA!! A terapia criativa transpessoal foi incrível. Foi uma
+                  experiência poderosa que me permitiu acessar partes de mim mesmo que eu nem sabia que existiam. me mais empoderada e
+                  confiante em minha jornada de autodescoberta. Obrigada por sua dedicação e habilidades como terapeuta!"
                 </p>
               </div>
             </div>
@@ -75,13 +76,14 @@ export default function App() {
                 <div className='content-profile'>
                   <div className='text-profile'>
                     <h3>Renata Ferreira da Silva</h3>
-                    <p>TERAPIA CRIATIVA TRANSPESSOAL - ADVOGADA - TRÊS PONTES/MG</p>
+                    <p>REIKI - ADVOGADA - PORTO SEGURO/BA</p>
                   </div>
                 </div>
                 <p className='profile-description'>
-                  "Vá, estou muito feliz por ter feito a terapia criativa transpessoal com você. Foi uma experiência única que me ajudou a
-                  olhar para dentro de mim de uma forma nova e profunda. Me sinto mais inspirada e motivada para explorar minha mente e
-                  emoções. Muito obrigada por compartilhar sua expertise e me ajudar em minha jornada de autoconhecimento!"
+                  "Oiiii Vá, estou emocionada com a experiência de Reiki que tive com você. Foi uma jornada de autocuidado e autocura que me
+                  ajudou a me reconectar com minha essência interior. Senti uma sensação profunda de paz e clareza durante a sessão, e
+                  continuo me sentindo mais equilibrada e centrada após a experiência. Muito obrigada por compartilhar sua habilidade e
+                  cuidado comigo!"
                 </p>
               </div>
             </div>
@@ -91,14 +93,16 @@ export default function App() {
               <div className='container-profile'>
                 <div className='content-profile'>
                   <div className='text-profile'>
-                    <h3>Rafaela Brito</h3>
-                    <p>TERAPIA CRIATIVA TRANSPESSOAL - DENTISTA - TRÊS PONTES/MG</p>
+                    <h3>Raquel Brito</h3>
+                    <p>REGISTROS AKASHICOS - DENTISTA - ARCOS/MG</p>
                   </div>
                 </div>
                 <p className='profile-description'>
-                  "Vá, estou muito feliz por ter feito a terapia criativa transpessoal com você. Foi uma experiência única que me ajudou a
-                  olhar para dentro de mim de uma forma nova e profunda. Me sinto mais inspirada e motivada para explorar minha mente e
-                  emoções. Muito obrigada por compartilhar sua expertise e me ajudar em minha jornada de autoconhecimento!"
+                  "Oi Valéria tudo bem? queria deixar aqui minha experiencia sobre a sessão, sou imensamente grata pela sessão que tive de
+                  leitura de registros akashicos. Você me ajudou a ver as coisas sob uma nova perspectiva e me libertou de padrões
+                  limitantes que me impediam de progredir. Senti uma sensação de leveza e alívio que não experimentava há muito tempo.
+                  Agora, estou mais confiante em minha capacidade de manifestar a vida que desejo. Muito obrigada, você é uma terapeuta
+                  maravilhosa."
                 </p>
               </div>
             </div>
@@ -108,14 +112,15 @@ export default function App() {
               <div className='container-profile'>
                 <div className='content-profile'>
                   <div className='text-profile'>
-                    <h3>Ingrid Larissa</h3>
-                    <p>TERAPIA CRIATIVA TRANSPESSOAL - EMPRESÁRIA - TRÊS PONTES/MG</p>
+                    <h3>Ingrid Oliveira</h3>
+                    <p>TARÔ - EMPRESÁRIA - SÃO PAULO/SP</p>
                   </div>
                 </div>
                 <p className='profile-description'>
-                  "Vá, estou muito feliz por ter feito a terapia criativa transpessoal com você. Foi uma experiência única que me ajudou a
-                  olhar para dentro de mim de uma forma nova e profunda. Me sinto mais inspirada e motivada para explorar minha mente e
-                  emoções. Muito obrigada por compartilhar sua expertise e me ajudar em minha jornada de autoconhecimento!"
+                  "Oi Valéria! Quero deixar meu depoimento aqui e expressar minha profunda gratidão pela sessão de leitura de tarô que tive
+                  com você. Foi uma experiência profundamente reveladora, e fiquei impressionada com sua habilidade de interpretar as cartas
+                  com tanta precisão. Me senti mais consciente dos padrões e desafios em minha vida, e agora estou mais confiante em minha
+                  capacidade de navegar pelas incertezas do futuro. Obrigada por seu trabalho maravilhoso!"
                 </p>
               </div>
             </div>
